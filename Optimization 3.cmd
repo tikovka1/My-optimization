1490a5c2b90e901be92e1b514c4cf184b94a25b7
aqmister <163766029+tikovka1@users.noreply.github.com>
2025-01-10 10:59:09 +0300

    Optimization
    Fps

---
+++ Optimization 3.cmd
@@ -0,0 +1,13 @@
+bcdedit /deletevalue useplatformclock
+bcdedit /set disabledynamictick yes
+bcdedit /set useplatformtick yes
+bcdedit /timeout 0
+bcdedit /set nx optout
+bcdedit /set bootux disabled
+bcdedit /set bootmenupolicy standard
+bcdedit /set hypervisorlaunchtype off
+bcdedit /set tpmbootentropy ForceDisable
+bcdedit /set quietboot yes
+bcdedit /set {globalsettings} custom:16000067 true
+bcdedit /set {globalsettings} custom:16000069 true
+bcdedit /set {globalsettings} custom:16000068 true
